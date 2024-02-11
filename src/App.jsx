6144291ba8cfe54255e30bb477a1b2e9d2d26152
Kaveesha <kavishajayasingha90@gmai.com>
2024-02-11 15:59:47 +0530

    some directory changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,96 +1,76 @@
-import './App.css'
-import { LeftMenu } from './Componenets/LeftMenu';
-import LoginSignup from './Componenets/LoginSignup/LoginSignup';
-import { MainContainer } from './Componenets/MainContainer';
-import { RightMenu } from './Componenets/RightMenu';
-import { createBrowserRouter, RouterProvider, Route, Link, Outlet } from 'react-router-dom';
-import Home from './Pages/Home';
-import Emotion from './Pages/Emotion';
-import Search from './Pages/Search';
-import ErrorPage from './Pages/ErrorPage';
-import Footer from './Componenets/Footer';
-import { Banner } from './Componenets/Banner';
-import Modal from './Componenets/modal/Modal';
+import "./App.css";
+import { LeftMenu } from "./Componenets/LeftMenu";
+import LoginSignup from "./Componenets/LoginSignup/LoginSignup";
+import { MainContainer } from "./Componenets/MainContainer";
+import { RightMenu } from "./Componenets/RightMenu";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+import Home from "./Pages/Home";
+import Emotion from "./Pages/Emotion";
+import Search from "./Pages/Search";
+import ErrorPage from "./Pages/ErrorPage";
+import Footer from "./Componenets/Footer";
+import { Banner } from "./Componenets/Banner";
+import Modal from "./Componenets/modal/Modal";
 
 const Dashboard = () => {
-  return(
+  return (
     <div>
-     <div className='tin'> 
-      <LeftMenu />
-      <Outlet/>
-      <RightMenu/>
-      
+      <div className="tin">
+        <LeftMenu />
+        <Outlet />
+        <RightMenu />
       </div>
-      <Footer/>
-      
-      
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element: <Dashboard/>,
-    children:[{
-      path:"/",
-      element:<Home/>,
-    },
-    {
-      path:"/emotion",
-      element:<Emotion/>
-    },
-    {
-      path:"/Search",
-      element:<Search/>
-    },
-    {
-      path:"/main",
-      element:<MainContainer/>
-    },
-   
-  ]
-  }, 
-  
-  
-  // {
-  //   path:"/emotion",
-  //   element:<Emotion/>
-  // },
-  // {
-  //   path:"/Search",
-  //   element:<Search/>
-  // },
-  {
-    path:"*",
-    element:<ErrorPage/>
+    path: "/",
+    element: <Dashboard />,
+    children: [
+      {
+        path: "/",
+        element: <MainContainer />,
+      },
+      {
+        path: "/emotion",
+        element: <Emotion />,
+      },
+      {
+        path: "/Search",
+        element: <Search />,
+      },
+      {
+        path: "/main",
+        element: <MainContainer />,
+      },
+    ],
   },
-])
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
+]);
 
 function App() {
-
-  return(
-
-  <>
+  return (
+    <>
   
-  {/* <div className="background">
-    <div className='App'>
-      <LeftMenu />
-      <MainContainer />
-      <RightMenu /> */}
-
       <div className="background">
-      
         <div className="App">
-        
-        <RouterProvider router={router}/>
-        
+          <RouterProvider router={router} />
         </div>
-       
       </div>
-      <Modal/>
-    {/* </div>
-    </div> */}
+      <Modal />
+    
     </>
   );
 }
