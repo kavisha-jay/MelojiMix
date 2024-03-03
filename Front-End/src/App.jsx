1fd4f6cb82b764945cd0050c55a31a773201d342
Kaveesha <kavishajayasingha90@gmai.com>
2024-03-04 01:47:28 +0530

    some changes

--- Front-End/src/App.jsx
+++ Front-End/src/App.jsx
@@ -14,42 +14,60 @@ import TopCharts from './pages/TopCharts';
 import back from './assets/back.jpg';
 import Modal from './components/modal/Modal';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
   const { activeSong } = useSelector((state) => state.player);
 
-  return (
-    <div className="w-full relative overflow-hidden flex z-10 flex-grow h-screen ">
-      <Sidebar />
-      <div className="background">
-      
-        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse bg-[rgba(34,34,34,0.6)]  backdrop-blur-3xl">
-          <div className="flex-1 h-fit pb-40 ">
-         
-          <Searchbar/>
+  const [calm, SetCalm] = useState([])
+  useEffect(() =>{
 
-            <Routes>
-              <Route path="/" element={ <Discover />} />
-              <Route path="/top-artists" element={<TopArtists />} />
-              <Route path="/top-charts" element={<TopCharts />} />
-              <Route path="/around-you" element={<AroundYou />} />
-              <Route path="/artists/:id" element={<ArtistDetails />} />
-              <Route path="/songs/:songid" element={<SongDetails />} />
-              <Route path="/search/:searchTerm" element={<Search />} />
-            </Routes>
-          </div>
-          <div className="xl:sticky relative top-0 h-fit">
-            <TopPlay />
-          </div>
-        </div>
-      </div>
-      <Modal/>
-      {activeSong?.title && (
-        <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
-          <MusicPlayer />
-        </div>
-      )}
+    axios.get('http://localhost:5000/Calm')
+    .then(Calm => SetCalm(Calm.data))
+    .catch(err => console.log(err))
+  }, [])
+
+  return (
+    <div>
+      {
+        calm.map(calm =>{
+          return <div>{calm.Songname}</div>
+        })
+      }
     </div>
+
+    // <div className="w-full relative overflow-hidden flex z-10 flex-grow h-screen ">
+    //   <Sidebar />
+    //   <div className="background">
+      
+    //     <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse bg-[rgba(34,34,34,0.6)]  backdrop-blur-3xl">
+    //       <div className="flex-1 h-fit pb-40 ">
+         
+    //       <Searchbar/>
+
+    //         <Routes>
+    //           <Route path="/" element={ <Discover />} />
+    //           <Route path="/top-artists" element={<TopArtists />} />
+    //           <Route path="/top-charts" element={<TopCharts />} />
+    //           <Route path="/around-you" element={<AroundYou />} />
+    //           <Route path="/artists/:id" element={<ArtistDetails />} />
+    //           <Route path="/songs/:songid" element={<SongDetails />} />
+    //           <Route path="/search/:searchTerm" element={<Search />} />
+    //         </Routes>
+    //       </div>
+    //       <div className="xl:sticky relative top-0 h-fit">
+    //         <TopPlay />
+    //       </div>
+    //     </div>
+    //   </div>
+    //   <Modal/>
+    //   {activeSong?.title && (
+    //     <div className="absolute h-28 bottom-0 left-0 right-0 flex animate-slideup bg-gradient-to-br from-white/10 to-[#2a2a80] backdrop-blur-lg rounded-t-3xl z-10">
+    //       <MusicPlayer />
+    //     </div>
+    //   )}
+    // </div>
   );
 };
 
